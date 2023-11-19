09dd247742388610f65f7bc4081a65ae6e6b4287
Meiyi <ant19950601@gmail.com>
2023-11-19 22:32:10 +1100

    :rocket: github page

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import linaria from '@linaria/rollup'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    linaria({
+      include: ['**/*.{ts,tsx}'],
+      babelOptions: {
+        presets: ['@babel/preset-typescript', '@babel/preset-react'],
+      },
+    }),
+  ],
+  base: "https://MeganxLi.github.io/coffee-guide"
 })
