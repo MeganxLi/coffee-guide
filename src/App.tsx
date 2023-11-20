ef4eadc0142f9f10c78b8c0e955485c7431160fb
Meiyi <ant19950601@gmail.com>
2023-11-20 22:51:27 +1100

    :lipstick: reset styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
+import { ResetStyles } from './styled/base/Reset'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Vite + React</h1>
-
-    </>
+    <div className={ResetStyles}>
+    </div>
   )
 }
 
