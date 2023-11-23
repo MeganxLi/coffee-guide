55c41771cfb35726d71206e583afc136af14889b
Meiyi <ant19950601@gmail.com>
2023-11-23 23:15:00 +1100

    :label: add base Typescript

---
+++ src/types/state.d.ts
@@ -0,0 +1,16 @@
+type LanguageListType = {
+  ios: string,
+  name: string
+}
+
+type CoffeeListType = {
+  name: string,
+  espresso: number,
+  milk?: number,
+  chocolate?: number,
+  milkFoam?: number,
+  milkSperm?: number,
+  water?: number,
+  syrup?: number,
+  butter?: number,
+}
