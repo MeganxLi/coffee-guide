77f0b1c4d662745633aafafc637cc82fa7d71bd4
Meiyi <ant19950601@gmail.com>
2023-11-25 23:23:24 +1100

    :wrench: vite config add vite-plugin-eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import linaria from '@linaria/rollup'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    eslintPlugin({
+      cache: false,
+      include: ['./src/**/*.js', './src/**/*.jsx'],
+      exclude: [],
+    }),
     linaria({
       include: ['**/*.{ts,tsx}'],
       babelOptions: {
@@ -13,5 +19,5 @@ export default defineConfig({
       },
     }),
   ],
-  base: "https://MeganxLi.github.io/coffee-guide"
+  base: 'https://MeganxLi.github.io/coffee-guide',
 })
