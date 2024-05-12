43a4429de7d565b66edf3e17201e3dfab39ce2f5
Meiyi <ant19950601@gmail.com>
2024-05-13 01:50:12 +1000

    :lipstick: Language list styled

---
+++ src/styled/components/LanguageMenu.ts
@@ -0,0 +1,21 @@
+import { styled } from '@linaria/react'
+
+export const LanguageButton = styled.button`
+`
+
+export const LanguageNavBlack = styled.nav`
+  padding: 0.5rem 1rem;
+
+  li{
+    padding: 0.5rem;
+    text-decoration: none;
+    color: #333333;
+    padding: 15px 20px;
+    display: block;
+    text-align: left;
+
+    &:hover{
+      background-color: pink;
+    }
+  }
+`
