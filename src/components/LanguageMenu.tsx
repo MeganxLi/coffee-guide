97bfacdc6ca698160a123e6643e9796fe0c22533
Meiyi <ant19950601@gmail.com>
2024-05-17 02:11:10 +1000

    :memo: Update Language button file

--- src/components/LanguageMenu.tsx
+++ src/components/LanguageMenu.tsx
@@ -1,43 +1,35 @@
-import { useRef, useState } from 'react'
-
 import * as Icon from 'react-feather'
 
 import LanguageList from '../constants/LanguageList'
-import { LanguageMenuBlock, LanguageNavBlock } from '../styled/components/LanguageMenu'
+import { LanguageButton, LanguageMenuBlock, LanguageNavBlock } from '../styled/components/LanguageMenu'
 
 interface Props {
   language: string,
   onChange: ((value: string) => void)
 }
 
-const LanguageMenu = ({ language, onChange }: Props) => {
-  const dropdownRef = useRef<HTMLDivElement>(null)
-  const [isActive, setIsActive] = useState(false)
-  const onClick = () => setIsActive(!isActive)
+const LanguageMenu = ({ language, onChange }: Props) => (
+  <LanguageMenuBlock>
+    <LanguageButton type="button">
+      <Icon.Globe />
+    </LanguageButton>
 
-  return (
-    <LanguageMenuBlock>
-      <button onClick={onClick} type="button">
-        <Icon.Globe />
-      </button>
-
-      <LanguageNavBlock ref={dropdownRef} show={isActive}>
-        <ul>
-          {LanguageList.map((item: LanguageListType) => (
-            <li
-              key={item.ios}
-              onClick={() => {
-                onChange(item.ios)
-              }}
-              className={language && '2'}
-            >
-              {item.name}
-            </li>
-          ))}
-        </ul>
-      </LanguageNavBlock>
-    </LanguageMenuBlock>
-  )
-}
+    <LanguageNavBlock className="language-nav">
+      <ul>
+        {LanguageList.map((item: LanguageListType) => (
+          <li
+            key={item.ios}
+            onClick={() => {
+              onChange(item.ios)
+            }}
+            className={language && '2'}
+          >
+            {item.name}
+          </li>
+        ))}
+      </ul>
+    </LanguageNavBlock>
+  </LanguageMenuBlock>
+)
 
 export default LanguageMenu
