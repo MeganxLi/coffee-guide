c4925610a8a474a776ff50d62ac26e685d00f5f8
Meiyi <ant19950601@gmail.com>
2024-05-26 22:55:25 +1000

    :lipstick: Update Coffee Colors parameter

--- src/styled/utils/color.ts
+++ src/styled/utils/color.ts
@@ -2,6 +2,7 @@ const Colors = {
   Black: {
     900: '#595959',
     700: '#727272',
+    600: '#817f75',
     200: '#E4E4E4',
     transparency: '#0000000d',
   },
@@ -10,8 +11,20 @@ const Colors = {
     transparency: '#5a778547',
   },
   White: {
+    900: '#fff',
     700: '#F2E5DA',
     500: '#F0F0F0',
+  },
+  Coffee: {
+    Espresso: '#4F2B0C',
+    Cafe: '#49362F',
+    Butter: '#FFF4E4',
+    Syrup: '#AC5214',
+    Water: '#A1D5EA',
+    MilkSperm: '#49362F',
+    Chocolate: '#301D0C',
+    Milk: '#FFEEC4',
+    MileFoam: '#FEF3E2',
   },
 }
 export default Colors
