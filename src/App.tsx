b5af961c5a70561831ee0d5b4dcaa062d3989849
Meiyi <ant19950601@gmail.com>
2024-05-27 00:25:22 +1000

    :memo: Update page

--- src/App.tsx
+++ src/App.tsx
@@ -23,15 +23,29 @@ import {
   MenuTitleStyle,
   WrapperStyle,
   SelectCoffeeStyle,
+  CoffeeCapFillingBorderStyle,
+  CoffeeCapFillingMaskStyle,
+  CoffeeEspressoStyle,
+  CoffeeMikeStyle,
+  CoffeeMileFoamStyle,
+  CoffeeButterStyle,
+  CoffeeSyrupStyle,
+  CoffeeMilkSpermStyle,
+  CoffeeWaterStyle,
+  CoffeeChocolateStyle,
 } from './styled/page'
 
 const App = () => {
   const { t } = useTranslation()
 
-  const [lang, setLang] = useState<string>(LanguageIosEnum.JA)
-  const [selectCoffee, setSelectCoffee] = useState<string>(CoffeeList[0].name)
+  const [lang, setLang] = useState<string>(LanguageIosEnum.EN)
+  const [selectCoffee, setSelectCoffee] = useState<CoffeeListType>(CoffeeList[0])
 
-  const clickCoffee = (coffeeName: string) => setSelectCoffee(coffeeName)
+  const clickCoffee = (coffeeName: string) => {
+    console.log(CoffeeList.filter((item) => item.name === coffeeName)[0])
+
+    setSelectCoffee(CoffeeList.filter((item) => item.name === coffeeName)[0])
+  }
   useEffect(() => {
     if (lang !== '') i18n.changeLanguage(lang)
   }, [lang])
@@ -48,8 +62,20 @@ const App = () => {
             <CoffeeCapLidStrawStyle />
             <CoffeeCapLidCoverStyle />
             <CoffeeCapLidMiddleStyle />
-            <CoffeeCapFillingStyle />
-            <SelectCoffeeStyle>{t(`Coffee.${selectCoffee}`)}</SelectCoffeeStyle>
+            <CoffeeCapFillingStyle>
+              <CoffeeCapFillingBorderStyle />
+              <CoffeeCapFillingMaskStyle>
+                <CoffeeChocolateStyle $flex_grow={selectCoffee.chocolate || 0} $light_color>{t('Ingredients.chocolate')}</CoffeeChocolateStyle>
+                <CoffeeEspressoStyle $flex_grow={selectCoffee.espresso || 0} $light_color>{t('Ingredients.espresso')}</CoffeeEspressoStyle>
+                <CoffeeSyrupStyle $flex_grow={selectCoffee.syrup?.number || 0} $light_color>{t(`Ingredients.${selectCoffee.syrup?.name}`)}</CoffeeSyrupStyle>
+                <CoffeeWaterStyle $flex_grow={selectCoffee.water || 0} $light_color>{t('Ingredients.water')}</CoffeeWaterStyle>
+                <CoffeeMilkSpermStyle $flex_grow={selectCoffee.milkSperm || 0}>{t('Ingredients.milkSperm')}</CoffeeMilkSpermStyle>
+                <CoffeeButterStyle $flex_grow={selectCoffee.butter || 0}>{t('Ingredients.butter')}</CoffeeButterStyle>
+                <CoffeeMikeStyle $flex_grow={selectCoffee.milk || 0}>{t('Ingredients.milk')}</CoffeeMikeStyle>
+                <CoffeeMileFoamStyle $flex_grow={selectCoffee.milkFoam || 0}>{t('Ingredients.milkFoam')}</CoffeeMileFoamStyle>
+              </CoffeeCapFillingMaskStyle>
+            </CoffeeCapFillingStyle>
+            <SelectCoffeeStyle>{t(`Coffee.${selectCoffee.name}`)}</SelectCoffeeStyle>
           </CoffeeCapStyle>
           <MenuStyle>
             <h4>Menu</h4>
@@ -58,7 +84,7 @@ const App = () => {
                 <MenuTitleStyle>{CoffeeListTitle[0]}</MenuTitleStyle>
                 {CoffeeList.map((item) => (
                   <MenuTextStyle
-                    selected={selectCoffee === item.name}
+                    selected={selectCoffee.name === item.name}
                     key={item.name}
                     onClick={() => clickCoffee(item.name)}
                   >
